e7f6ee22e0c4da67d35a68c55fc699aae6347d51
vijayasrikarPORALLA <_VPoralla.AZ@sodexo.com>
2022-10-26 12:49:48 +0200

    add transaction into data.json file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # transactions
+
 to add all trasactions into json file
+
+TODO:
+1: replace transaction if already exist
--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+let fs = require("fs");
+let dataFile = "./data.json";
+let obj = {
+  transactions: [],
+};
+let json = [];
 
+fs.readFile("data.json", "utf8", function readFileCallback(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    if (data.length !== 0) {
+      obj = JSON.parse(data);
+    }
+    obj.transactions.push({ id: 1, date: 26102022, amount: 7888 });
+    json = JSON.stringify(obj);
+    fs.writeFile("data.json", json, "utf8", function (err) {
+      if (!err) {
+        console.log("OK");
+      }
+    });
+  }
+});
